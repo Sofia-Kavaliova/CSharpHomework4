018ed28f2641dbf2b06320b875ab355faeb291fd
Sofia <kovaleva.sonia.02@mail.ru>
2023-10-19 14:45:39 +0300

    Add decision exercise1

---
+++ Exercise001/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int PowNumber(int a, int b)
+{
+    int res=1;
+    for(int i=0; i<b; i++)
+    {
+        res*=a;
+    }
+    return res;
+}
+//------------------
+int numberA = ReadInt("Введите первое число: ");
+int numberB = ReadInt("Введите второе число: ");
+Console.WriteLine(PowNumber(numberA, numberB));
